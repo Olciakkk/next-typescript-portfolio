78f2058307305517ea4985a2f91eeb63f6d63a03
Olciak <72775937+Olciakkk@users.noreply.github.com>
2020-10-13 07:05:05 +0200

    refactor(social media): use more declarative way

--- components/footer/icon-button.tsx
+++ components/footer/icon-button.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { Button, Tooltip } from 'antd'
 
-export interface IconButtonProps {
+export const IconButton: React.FC<{
   url: string
   title: string
-  icon: JSX.Element
-}
-export const IconButton: React.FC<IconButtonProps> = ({ url, title, icon }) => (
+}> = ({ url, title, children }) => (
   <Tooltip placement="top" title={title}>
     <Button
       type="primary"
       icon={
         <a href={url} target="_blank" rel="noopener noreferrer">
-          {icon}
+          {children}
         </a>
       }
     />
--- components/footer/social-media.tsx
+++ components/footer/social-media.tsx
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/styles'
 import React from 'react'
-import { IconButton, IconButtonProps } from './icon-button'
+import { IconButton } from './icon-button'
 import { GithubOutlined, LinkedinOutlined, MediumOutlined } from '@ant-design/icons'
 
 const useStyles = makeStyles(() => ({
@@ -12,34 +12,23 @@ const useStyles = makeStyles(() => ({
   },
 }))
 
-const icons: IconButtonProps[] = [
-  {
-    title: 'GitHub',
-    url: 'https://github.com/PavelSrom',
-    icon: <GithubOutlined />,
-  },
-  {
-    title: 'LinkedIn',
-    url: 'https://www.linkedin.com/in/pavel-srom-3011a8171',
-    icon: <LinkedinOutlined />,
-  },
-  {
-    title: 'Medium',
-    url: 'https://medium.com/@srompavel98',
-    icon: <MediumOutlined />,
-  },
-]
-
-export const SocialMedia: React.FC<{
-
-}> = () => {
+export const SocialMedia: React.FC = () => {
   const classes = useStyles()
 
   return (
     <div className={classes.iconBox}>
-      {icons.map((content, index) => (
-        <IconButton key={index} {...content} />
-      ))}
+      <IconButton title="GitHub" url="https://github.com/PavelSrom">
+          <GithubOutlined />
+        </IconButton>
+        <IconButton
+          title="LinkedIn"
+          url="https://www.linkedin.com/in/pavel-srom-3011a8171"
+        >
+          <LinkedinOutlined />
+        </IconButton>
+        <IconButton title="Medium" url="https://medium.com/@srompavel98">
+          <MediumOutlined />
+        </IconButton>
     </div>
   )
 }
